9725ec1bc774c32331a124852de51770cef7b1a5
KAWASAKI Yasukazu <kawasaki@dev.kawa1128.jp>
2016-12-22 20:01:40 +0000

    PostProcess

---
+++ src/constants.h
@@ -0,0 +1,17 @@
+#ifndef __CONSTANTS_H_
+#define __CONSTANTS_H_
+
+#define NX 299
+#define NY 269
+#define CLAT 35.428
+#define CLON 137.302
+#define CX 150
+#define CY 135
+#define DX 10000
+#define DY 10000
+
+#define TMAX 85
+#define ERROR 1.e30
+
+
+#endif
